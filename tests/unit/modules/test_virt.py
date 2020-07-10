d7b67adab708bf3a4718b9da7056219a60f23b4f
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-07-10 16:19:32 +0200

    Cleanup virt boot parameters update test
    In order to reduce the number of lines of the test, removed the XML
    definition in the boot parameters update that is not used, i.e. the
    devices definitions.
    
    Also change some variable in the test to have coherent naming

--- tests/unit/modules/test_virt.py
+++ tests/unit/modules/test_virt.py
@@ -1815,13 +1815,13 @@ class VirtTestCase(TestCase, LoaderModuleMockMixin):
         self.assertEqual(setxml.find("vcpu").text, "2")
         self.assertEqual(setvcpus_mock.call_args[0][0], 2)
 
+        # Update boot parameter case
         boot = {
             "kernel": "/root/f8-i386-vmlinuz",
             "initrd": "/root/f8-i386-initrd",
             "cmdline": "console=ttyS0 ks=http://example.com/f8-i386/os/",
         }
 
-        # Update with boot parameter case
         self.assertEqual(
             {
                 "definition": True,
@@ -1845,7 +1845,7 @@ class VirtTestCase(TestCase, LoaderModuleMockMixin):
             "console=ttyS0 ks=http://example.com/f8-i386/os/",
         )
 
-        boot_uefi = {
+        uefi_boot = {
             "loader": "/usr/share/OVMF/OVMF_CODE.fd",
             "nvram": "/usr/share/OVMF/OVMF_VARS.ms.fd",
         }
@@ -1856,7 +1856,7 @@ class VirtTestCase(TestCase, LoaderModuleMockMixin):
                 "disk": {"attached": [], "detached": [], "updated": []},
                 "interface": {"attached": [], "detached": []},
             },
-            virt.update("my_vm", boot=boot_uefi),
+            virt.update("my_vm", boot=uefi_boot),
         )
         setxml = ET.fromstring(define_mock.call_args[0][0])
         self.assertEqual(
@@ -2484,48 +2484,6 @@ class VirtTestCase(TestCase, LoaderModuleMockMixin):
                 <loader>/usr/share/old/OVMF_CODE.fd</loader>
                 <nvram>/usr/share/old/OVMF_VARS.ms.fd</nvram>
               </os>
-              <devices>
-                <disk type='file' device='disk'>
-                  <driver name='qemu' type='qcow2'/>
-                  <source file='{0}{1}vm_with_boot_param_system.qcow2'/>
-                  <backingStore/>
-                  <target dev='vda' bus='virtio'/>
-                  <alias name='virtio-disk0'/>
-                  <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
-                </disk>
-                <disk type='file' device='disk'>
-                  <driver name='qemu' type='qcow2'/>
-                  <source file='{0}{1}vm_with_boot_param_data.qcow2'/>
-                  <backingStore/>
-                  <target dev='vdb' bus='virtio'/>
-                  <alias name='virtio-disk1'/>
-                  <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x1'/>
-                </disk>
-                <interface type='network'>
-                  <mac address='52:54:00:39:02:b1'/>
-                  <source network='default' bridge='virbr0'/>
-                  <target dev='vnet0'/>
-                  <model type='virtio'/>
-                  <alias name='net0'/>
-                  <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
-                </interface>
-                <interface type='network'>
-                  <mac address='52:54:00:39:02:b2'/>
-                  <source network='oldnet' bridge='virbr1'/>
-                  <target dev='vnet1'/>
-                  <model type='virtio'/>
-                  <alias name='net1'/>
-                  <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x1'/>
-                </interface>
-                <graphics type='spice' port='5900' autoport='yes' listen='127.0.0.1'>
-                  <listen type='address' address='127.0.0.1'/>
-                </graphics>
-                <video>
-                  <model type='qxl' ram='65536' vram='65536' vgamem='16384' heads='1' primary='yes'/>
-                  <alias name='video0'/>
-                  <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
-                </video>
-              </devices>
             </domain>
         """
         domain_mock_boot = self.set_mock_vm("vm_with_boot_param", xml_boot)
